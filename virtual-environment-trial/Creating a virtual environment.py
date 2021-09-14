b550406671288c51223aa42607a2fedde5e83a95
symonkipkemei <symonkipkemei@gmail.com>
2021-09-14 16:56:54 +0300

    virtual environment

---
+++ virtual-environment-trial/Creating a virtual environment.py
@@ -0,0 +1,24 @@
+# STEP 0
+# virtual env tool is already installed, it comes pre installed with python 3
+
+
+# STEP 1
+# First create a project directory you want to install the virtual environment
+# mkdir
+# clone a repository from github
+
+# STEP 2
+# python3 -m venv env
+# virtualenv env
+"""creates a directory called env which contains all the tools for virtual environment"""
+
+# STEP 3
+# env\Scripts\activate
+""" activates the virtual environment created
+Note that we use backslash and scripts is titled whereas activate is small letters"""
+
+# STEP 4
+# deactivate
+
+# STEP 5
+# you can now install what you want within the virtual environment
